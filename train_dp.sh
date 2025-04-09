90a01757ab1ad2ebe44475988d3203c9c414d768
Wendi Chen <chenwendi-andy@sjtu.edu.cn>
2025-04-09 20:17:15 +0800

    [fix] dataset in DP training script

--- train_dp.sh
+++ train_dp.sh
@@ -3,6 +3,6 @@
 CUDA_VISIBLE_DEVICES=0 accelerate launch train.py \
     --config-name=train_diffusion_unet_real_image_workspace \
     task=real_peel_image_gelsight_emb_absolute_12fps \
-    task.dataset_path=/home/wendi/Desktop/record_data/peel_v3_downsample1_zarr \
+    task.dataset_path=/home/wendi/Desktop/record_data/peel_v3_downsample2_zarr \
     task.name=real_peel_image_gelsight_emb_absolute_12fps \
     logging.mode=online
